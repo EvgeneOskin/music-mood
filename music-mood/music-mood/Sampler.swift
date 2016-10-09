05c42a4974650d015da42f79e1b6624c0e05b941
EvgeneOskin <eoskin@crystalnix.com>
2016-10-09 15:04:01 +0600

    Add ambient sounds and balance sound for its.
    Add preferences and default resources for play.

--- music-mood/music-mood/Sampler.swift
+++ music-mood/music-mood/Sampler.swift
@@ -9,45 +9,52 @@
 import Foundation
 import AudioKit
 
-func instrument(noteNumber: Int, rate: AKParameter, amplitude: AKParameter) -> AKOperation {
-    let metro = AKOperation.metronome(frequency: 82.0 / (60.0 * rate))
-    let frequency = noteNumber.midiNoteToFrequency()
-    return AKOperation.fmOscillator(baseFrequency: frequency, amplitude: amplitude)
-        .triggeredWithEnvelope(trigger: metro, attack: 1.5, hold: 1, release: 0.5)
+
+func resourcePlayer(resource: String) -> AKAudioPlayer {
+    let fileUrl = Bundle.main.resourcePath
+    let file = try! AKAudioFile(readFileName: resource, baseDir: AKAudioFile.BaseDirectory.resources)
+    let player  = try! AKAudioPlayer(file: file)
+    player.looping  = true
+    return player
 }
 
 class Sampler {
+    var mix: AKMixer!
+    var cooldown: AKAudioPlayer!
+    var warmup: AKAudioPlayer!
     
-    let generator = AKOperationGenerator() { parameters in
-        let multiply = parameters[0]
-        
-        let instrument1 = instrument(noteNumber: 60, rate: 4 * multiply, amplitude: 0.5)
-        let instrument2 = instrument(noteNumber: 62, rate: 5 * multiply, amplitude: 0.4)
-        let instrument3 = instrument(noteNumber: 65, rate: 7 * multiply, amplitude: 1.3 / 4.0)
-        let instrument4 = instrument(noteNumber: 67, rate: 7 * multiply, amplitude: 0.125)
-        let chaos = AKOperation.whiteNoise() * 0.05 / multiply;
-        
-        let instruments = (chaos + instrument1 + instrument2 + instrument3 + instrument4) * 0.13
-        
-        let reverb = instruments.reverberateWithCostello(feedback: 0.8, cutoffFrequency: 5000).toMono()
-        
-        let mix = mixer(instruments, reverb, balance: 0.4)
-        return mix
+    let warmupResource = "cafe.mp3"
+    let cooldownResource = "rain.mp3"
+
+    func modeScaleToFrequence(_ frequency: Double) -> Double {
+        return exp(-pow(frequency - 50, 2) / 50);
     }
     
     init() {
-        AudioKit.output = generator
+        cooldown = resourcePlayer(resource: cooldownResource)
+        warmup = resourcePlayer(resource: warmupResource)
+        mix =  AKMixer(cooldown, warmup)
+        
+        AudioKit.output = mix
         AudioKit.start()
         
         reset();
-        generator.start();
+        cooldown.start();
+        warmup.start();
     }
     
     func change(frequency: Double) {
-        generator.parameters[0] = frequency;
+        let mode = modeScaleToFrequence(frequency);
+        warmup.volume = 1 - mode;
+        cooldown.volume = mode;
     }
     
     func reset() {
-        generator.parameters[0] = 1;
+        warmup.volume = 1;
+        cooldown.volume = 0;
+    }
+    
+    func setupTrack() {
+        
     }
 }
